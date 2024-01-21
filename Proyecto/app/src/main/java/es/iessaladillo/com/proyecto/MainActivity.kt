6e7239021c9c101c03817483032bc28e5bbd989b
Dr4u95 <j.4ntonio.28@gmail.com>
2024-01-21 23:56:24 +0100

    Commit #1.1 Modo Oscuro
    Probando modo oscuro basico

--- Proyecto/app/src/main/java/es/iessaladillo/com/proyecto/MainActivity.kt
+++ Proyecto/app/src/main/java/es/iessaladillo/com/proyecto/MainActivity.kt
@@ -1,6 +1,9 @@
 package es.iessaladillo.com.proyecto
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import es.iessaladillo.com.proyecto.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +19,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUpViews() {
         binding.txt1.text = "Hola Mundo"
+        binding.swEjemplo.setOnCheckedChangeListener{_,isSelected ->
+            if(isSelected){
+                enableDarkMode()
+            }else{
+                disableDarkMode()
+            }
+        }
+    }
+
+    private fun enableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+        delegate.applyDayNight()
+    }
+
+    private fun disableDarkMode(){
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+        delegate.applyDayNight()
     }
 }
