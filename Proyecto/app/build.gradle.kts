4bd2e4878d4160ac881e0bf27af88c1448383f7b
Dr4u95 <j.4ntonio.28@gmail.com>
2024-01-21 23:25:06 +0100

    Commit #1
    - Actualización Sdk.
    - Integración viewBinding al proyecto.

--- Proyecto/app/build.gradle.kts
+++ Proyecto/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "es.iessaladillo.com.proyecto"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "es.iessaladillo.com.proyecto"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,11 +33,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- Proyecto/app/src/main/java/es/iessaladillo/com/proyecto/MainActivity.kt
+++ Proyecto/app/src/main/java/es/iessaladillo/com/proyecto/MainActivity.kt
@@ -1,11 +1,20 @@
 package es.iessaladillo.com.proyecto
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import es.iessaladillo.com.proyecto.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpViews()
+    }
+
+    private fun setUpViews() {
+        binding.txt1.text = "Hola Mundo"
     }
 }
